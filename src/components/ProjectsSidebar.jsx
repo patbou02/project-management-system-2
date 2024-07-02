a5a460e1783c7afdb790ee0493acfa3482fadcd8
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-07-02 14:55:07 -0700

    Created boilerplate ProjectsSidebar component and imported it into the App root component.

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,13 @@
+function ProjectSidebar() {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
+
+export default ProjectSidebar;
