1ec4a7567e58bd7d02d3b546cfcca6a6780c770f
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-07-02 17:14:58 -0700

    Set State Object, event handler function and conditionally generated content based on State value. Added Props to ProjectSidebar and NoProjectSelected components to pass down event handler for when button is clicked.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+import { useState } from 'react';
+
 import ProjectSidebar from './components/ProjectsSidebar.jsx';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 
 function App() {
+
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  const handleStartAddProject = () => {
+    setProjectsState(prevState => {
+      return { ...prevState, selectedProjectId: null };
+    });
+  };
+
+  let content;
+
+  if (projectsState.selectedProjectId === undefined) {
+    // UNDEFINED => We are 'doing nothing'.
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  } else if (projectsState.selectedProjectId === null) {
+    // NULL => We are 'adding a new project'.
+    content = <NewProject />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NewProject />
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,6 +1,8 @@
 import Input from './Input.jsx';
 
 export default function NewProject() {
+  console.info('NewProject() - Adding new project');
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
