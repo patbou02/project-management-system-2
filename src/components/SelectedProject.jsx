73eed640bf414d211797337dc3600f79e1c732ba
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-07-04 14:14:30 -0700

    Created new Tasks component with basic styling and elements and imported it into the SelectedProjects component.

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,4 +1,4 @@
-  console.log('SelectedProject', project)
+import Tasks from './Tasks.jsx';
 
 export default function SelectedProject({ project, onDelete }) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
@@ -11,13 +11,13 @@ export default function SelectedProject({ project, onDelete }) {
     <div className="w-[35rem] mt-16">
       <header className="pb-4 mb-4 border-b-2 border-stone-300">
         <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title} <span className="text-lg">(ID: {project.id})</span></h1>
+          <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title} <span className="text-lg font-normal">(ID: {project.id})</span></h1>
           <button className="text-stone-600 hover:text-stone-950" onClick={onDelete}>Delete</button>
         </div>
         <p className="mb-4 text-stone-400">{formattedDate}</p>
         <p className="text-stone-600">{project.description}</p>
       </header>
-      TASKS
+      <Tasks />
     </div>
   );
 }
