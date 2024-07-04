4f054616524ab513ea7eaf243b41449f2f408810
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-07-04 00:55:27 -0700

    Added styling to the SelectedProject component using TailWindCSS classes. Added formattedDate function and renamed Prop name.

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,6 +1,6 @@
 import Button from './Button.jsx';
 
-function ProjectSidebar({ onStartAddProject, projects, onSelectedProject }) {
+function ProjectSidebar({ onStartAddProject, projects, onSelectProject }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
@@ -12,7 +12,7 @@ function ProjectSidebar({ onStartAddProject, projects, onSelectedProject }) {
           <li key={project.id}>
             <button
               className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800"
-              onClick={() => onSelectedProject(project)}
+              onClick={() => onSelectProject(project)}
             >{project.title}</button>
           </li>
         ))}
--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,14 +1,21 @@
 export default function SelectedProject({ project }) {
   console.log('SelectedProject', project)
+
+  const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  });
+
   return (
-    <div>
-      <header>
-        <div>
-          <h1>{project.title}</h1>
-          <button>Delete</button>
+    <div className="w-[35rem] mt-16">
+      <header className="pb-4 mb-4 border-b-2 border-stone-300">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
+          <button className="text-stone-600 hover:text-stone-950">Delete</button>
         </div>
-        <p>{project.dueDate}</p>
-        <p>{project.description}</p>
+        <p className="mb-4 text-stone-400">{formattedDate}</p>
+        <p className="text-stone-600">{project.description}</p>
       </header>
       TASKS
     </div>
